75267676798d3db32669f57846e1181a1af3a9e3
Javier <javier.martinezrom@gmail.com>
2025-07-23 17:55:24 +0100

    Variety of exercises using Lists (arrays) manipulation methods and iteration

---
+++ Part 3/IndexOfSmallest.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOfSmallest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // implement here a program that reads user input
+        // until the user enters 9999
+        
+        // after that, the program prints the smallest number
+        // and its index -- the smallest number
+        // might appear multiple times
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 9999) {
+                break;
+            }
+
+            list.add(input);
+        }
+        
+        System.out.println("");
+
+        int smallest = list.get(0);
+        for(int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (smallest > number) {
+                smallest = number;
+            }
+            System.out.println("Smallest number: " + smallest);
+            System.out.println("Found at index: " + i);
+        }
+    }
+}
+
+// Model answer
+// public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+ 
+//         // implement here a program that reads user input
+//         // until the user enters 9999
+        
+//         // after that, the program prints the smallest number
+//         // and its index -- the smallest number
+//         // might appear multiple times
+ 
+        
+//         ArrayList<Integer> list = new ArrayList<>();
+//         while (true) {
+//             int input = Integer.valueOf(scanner.nextLine());
+//             if (input == 9999) {
+//                 break;
+//             }
+ 
+//             list.add(input);
+//         }
+ 
+//         System.out.println("");
+ 
+//         int smallest = list.get(0);
+//         int index = 0;
+//         while (index < list.size()) {
+//             if (list.get(index) < smallest) {
+//                 smallest = list.get(index);
+//             }
+ 
+//             index++;
+//         }
+        
+//         System.out.println("Smallest number: " + smallest);
+ 
+//         index = 0;
+//         while (index < list.size()) {
+//             if (list.get(index) == smallest) {
+//                 System.out.println("Found at index: " + index);
+//             }
+ 
+//             index++;
+//         }
+ 
+//     }
