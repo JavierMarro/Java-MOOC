1ba5cea4a3c798de29bd4489a1cd65dddfd7ca5c
Javier <javier.martinezrom@gmail.com>
2025-08-01 11:16:52 +0100

    Classic ToDo list task with Java

---
+++ JavaMOOCI/Part6/Exercise10/TodoList.java
@@ -0,0 +1,27 @@
+package JavaMOOCI.Part6.Exercise10;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TodoList {
+
+    private ArrayList<String> taskList;
+
+    public TodoList(){
+        this.taskList = new ArrayList<>();
+    }
+
+    public void add(String task){
+        this.taskList.add(task);
+    }
+
+    public void print(){
+        for (int i = 0; i < this.taskList.size(); i++){
+            System.out.println((i + 1) + ": " + this.taskList.get(i));
+        }
+    }
+
+    public void remove(int number){
+        this.taskList.remove(number - 1);
+    }
+}
+
