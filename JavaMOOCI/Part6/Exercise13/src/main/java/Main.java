d395d4b6f25a9f34e8748bb06c20ea68c1b741e9
Javier <javier.martinezrom@gmail.com>
2025-08-15 18:24:53 +0100

    Exercise 13 creating a small Task management program via TDD with JUnit (which isn't configured currently in my VS Code)

---
+++ JavaMOOCI/Part6/Exercise13/src/main/java/Main.java
@@ -0,0 +1,14 @@
+package JavaMOOCI.Part6.Exercise13.src.main.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    // update here your exercise progress
+    public static int partsCompleted() {
+        return 2;
+    }
+
+}
---
+++ JavaMOOCI/Part6/Exercise13/src/test/java/Before.java
@@ -0,0 +1,5 @@
+package JavaMOOCI.Part6.Exercise13.src.test.java;
+
+public @interface Before {
+
+}
