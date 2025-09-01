542438c3b77f794c51f91d507dff6a5b9415ccfc
Javier <javier.martinezrom@gmail.com>
2025-09-01 15:02:12 +0100

    Manipulating Hash maps + getOrDefault method

---
+++ JavaMOOC2/Part8/Exercise10/IOU.java
@@ -0,0 +1,34 @@
+package JavaMOOC2.Part8.Exercise10;
+
+import java.util.HashMap;
+
+public class IOU {
+
+    private HashMap<String, Double> debt;
+    
+    public IOU(){
+        this.debt = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount){
+        if (this.debt.containsKey(toWhom)){
+            double total = this.debt.get(toWhom);
+            total+= amount;
+        } else {
+            this.debt.put(toWhom, amount);
+        }
+    }
+
+    public double howMuchDoIOweTo(String toWhom){
+        if (!this.debt.containsKey(toWhom)){
+            return 0;
+        } else {
+            return this.debt.get(toWhom);
+        }
+    }
+}
+
+// model answer for second method using getOrDefault method
+// public double howMuchDoIOweTo(String toWhom) {
+//         return debt.getOrDefault(toWhom, 0.0);
+//     }
---
+++ JavaMOOC2/Part8/Exercise10/Main.java
@@ -0,0 +1,16 @@
+package JavaMOOC2.Part8.Exercise10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Test your program here provided from UoH
+
+        IOU mattsIOU = new IOU();
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Michael", 30);
+
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+        System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
+    }
+}
+
