4a4165c20e7b2097e62259b1e183b6b541a50fdc
Javier <javier.martinezrom@gmail.com>
2025-09-26 17:22:24 +0100

    implemented the exact same way as Dog

--- JavaMOOC2/Part9/Exercise12ObjectPolymorphism/Cat.java
+++ JavaMOOC2/Part9/Exercise12ObjectPolymorphism/Cat.java
@@ -1,5 +1,17 @@
 package JavaMOOC2.Part9.Exercise12ObjectPolymorphism;
 
-public class Cat implements Animal{
+public class Cat extends Animal {
+
+    public Cat(){
+        this("Cat");
+    }
+    public Cat(String name){
+        super(name);
+    }
+
+    public void purr() {
+    System.out.println(super.getName() + " purrs");
+  }
     
 }
+
