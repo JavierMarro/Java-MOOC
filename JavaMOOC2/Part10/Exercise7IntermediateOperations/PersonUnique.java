c762dae1e682bc51b5eb344e2cfc1dab7d2fa998
Javier <javier.martinezrom@gmail.com>
2025-10-01 17:02:32 +0100

    Intermediate exercise using distinct() and sort() + extra information about TreeSet

--- JavaMOOC2/Part10/Exercise7IntermediateOperations/PersonUnique.java
+++ JavaMOOC2/Part10/Exercise7IntermediateOperations/PersonUnique.java
@@ -2,12 +2,15 @@ package JavaMOOC2.Part10.Exercise7IntermediateOperations;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class PersonUnique {
 
     // Given by UoH
     public static void main(String[] args) {
-        ArrayList<Person> persons = new ArrayList<>();
+        ArrayList<Person> people = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -29,10 +32,25 @@ public class PersonUnique {
             int birthYear = 0;
             if(scanner.hasNextLine()){birthYear = Integer.valueOf(scanner.nextLine());}
 
-            persons.add(new Person(firstName, lastName, birthYear));
+            people.add(new Person(firstName, lastName, birthYear));
             System.out.println("");
         }
 
         // My solution: Implement the printing of the unique last names in alphabetical order here:
+        Set<String> uniqueLastNames = people.stream()
+            .map(Person::getLastName)
+            .collect(Collectors.toCollection(TreeSet::new));
+
+        uniqueLastNames.forEach(System.out::println);
+
+
+        // Extra information found online about TreeSet for refactoring this task
+        // Set<String> uniqueLastNames = people.stream()
+        //     .map(Person::getLastName)
+        //     .collect(Collectors.toCollection(TreeSet::new));
+
+        // uniqueLastNames.forEach(System.out::println);
+        // A TreeSet in Java is a type of Set (so it stores unique elements, no duplicates allowed) but with the added property that it keeps the elements sorted in their “natural order” (or by a custom comparator you provide).
+
     }
 }
