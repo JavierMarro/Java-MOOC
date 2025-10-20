cba71031dccd4f86b4ae418ed0e74dd017050cb0
Javier <javier.martinezrom@gmail.com>
2025-10-20 17:42:56 +0100

    Part 2 implemented and part 1 improved to cover edge cases

--- JavaMOOC2/Part11/Exercise13DataToFiles/dictionary/SaveableDictionary.java
+++ JavaMOOC2/Part11/Exercise13DataToFiles/dictionary/SaveableDictionary.java
@@ -10,18 +10,28 @@ public class SaveableDictionary {
         this.dictionary = new HashMap<>();
     }
 
-    public void add(String word, String translation){
+    public void add(String words, String translation){
         // method putIfAbsent already ensures that the same word cannot be added twice
-        this.dictionary.putIfAbsent(word, translation);
-
+        // the method works either way for word added first or translation added first
+        this.dictionary.putIfAbsent(words, translation);
+        this.dictionary.putIfAbsent(translation, words);
     }
 
     public String translate(String word){
-        if(!this.dictionary.containsKey(word)){
+        //checking that the word is not found either as a key or a value (as a word or its translation)
+        if(!this.dictionary.containsKey(word) || !this.dictionary.containsValue(word)){
             return null;
         }
         
         return this.dictionary.get(word);
     }
+
+    // Part 2
+    public void delete(String word){
+        // Entry can be removed by either translation or word if found
+        this.dictionary.remove(word);
+        this.dictionary.remove(this.dictionary.get(word));
+    }
 }
+
 
