d1b89133e975801ba52e6ac9355dcc0da4857481
Javier <javier.martinezrom@gmail.com>
2025-10-20 18:45:25 +0100

    Added model answer

--- JavaMOOC2/Part11/Exercise13DataToFiles/dictionary/SaveableDictionary.java
+++ JavaMOOC2/Part11/Exercise13DataToFiles/dictionary/SaveableDictionary.java
@@ -1,15 +1,19 @@
 package JavaMOOC2.Part11.Exercise13DataToFiles.dictionary;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 public class SaveableDictionary {
-    // Part 1 + 3: Setting up the class, fields and constructor + 2 methods (add, translate)   
+    // Part 1 + 3: Setting up the class, fields and constructor  
     private HashMap<String, String> dictionary;
     private String fileName;
 
@@ -22,6 +26,7 @@ public class SaveableDictionary {
         this.fileName = file;
     }
 
+    // Part 1
     public void add(String words, String translation){
         // method putIfAbsent already ensures that the same word cannot be added twice
         // the method works either way for word added first or translation added first
@@ -89,4 +94,52 @@ public class SaveableDictionary {
     }
 }
 
+// Part 2 Model answer
+// public void delete(String word) {
+//         String translation = translate(word);
+ 
+//         words.remove(word);
+//         words.remove(translation);
+//     }
 
+// Part 3 Model answer
+// public boolean load() {
+//         try {
+//             Files.lines(Paths.get(this.file))
+//                     .map(l -> l.split(":"))
+//                     .forEach(parts -> {
+//                         this.words.put(parts[0], parts[1]);
+//                         this.words.put(parts[1], parts[0]);
+//                     });
+//             return true;
+//         } catch (IOException ex) {
+//             return false;
+//         }
+//     }
+
+// Part 4 Model answer
+// public boolean save() {
+//         try {
+//             PrintWriter writer = new PrintWriter(new File(file));
+//             saveWords(writer);
+//             writer.close();
+//         } catch (Exception e) {
+//             return false;
+//         }
+//         return true;
+//     }
+
+//     private void saveWords(PrintWriter writer) throws IOException {
+//         List<String> allreadySaved = new ArrayList<>();
+//         words.keySet().stream().forEach(word -> {
+//             if (allreadySaved.contains(word)) {
+//                 return;
+//             }
+ 
+//             String pari = word + ":" + words.get(word);
+//             writer.println(pari);
+ 
+//             allreadySaved.add(word);
+//             allreadySaved.add(words.get(word));
+//         });
+//     }
