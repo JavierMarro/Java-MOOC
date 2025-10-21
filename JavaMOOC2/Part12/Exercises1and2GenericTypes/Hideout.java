81919a397dcaba5efd7986dd961c6cecdd0e937d
Javier <javier.martinezrom@gmail.com>
2025-10-21 12:37:09 +0100

    Exercise practising generic typed class

---
+++ JavaMOOC2/Part12/Exercises1and2GenericTypes/Hideout.java
@@ -0,0 +1,56 @@
+package JavaMOOC2.Part12.Exercises1and2GenericTypes;
+
+public class Hideout<T> {
+    
+    private T item;
+
+    public Hideout(){
+        // empty constructor needed for this generic typed class?
+    }
+
+    public void putIntoHideout(T toHide){
+        // works as a setter
+        this.item = toHide;
+    }
+
+    public T takeFromHideout(){
+        if(this.item.equals(null)){
+            return null;
+        }
+        // returns value in class
+        T hiddenItem = this.item;
+        // then empties the hideout
+        this.item = null;
+        return hiddenItem;
+    }
+
+    public boolean isInHideout(){
+        // if the value of this class is null, then it's empty
+        if(this.item.equals(null)){
+            return false;
+        }
+        // otherwise it isn't empty
+        return true;
+    }
+
+}
+
+//Model answer
+// public class Hideout<T> {
+ 
+//     private T inHiding;
+ 
+//     public void putIntoHideout(T toHide) {
+//         this.inHiding = toHide;
+//     }
+ 
+//     public boolean isInHiding() {
+//         return this.inHiding != null;
+//     }
+ 
+//     public T takeFromHideout() {
+//         T previouslyInHiding = this.inHiding;
+//         this.inHiding = null;
+//         return previouslyInHiding;
+//     }
+// }
---
+++ JavaMOOC2/Part12/Exercises1and2GenericTypes/MainHideout.java
@@ -0,0 +1,30 @@
+package JavaMOOC2.Part12.Exercises1and2GenericTypes;
+
+import java.util.Scanner;
+
+public class MainHideout {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you may try out your class here (by UoH)
+        Hideout<String> den = new Hideout<>();
+        System.out.println(den.isInHideout());
+        den.putIntoHideout("peekaboo");
+        System.out.println(den.isInHideout());
+        System.out.println(den.takeFromHideout());
+        System.out.println(den.isInHideout());
+        den.putIntoHideout("toodaloo");
+        den.putIntoHideout("heelloo");
+        System.out.println(den.isInHideout());
+        System.out.println(den.takeFromHideout());
+        System.out.println(den.isInHideout());
+
+        Hideout<Integer> den2 = new Hideout<>();
+        System.out.println(den2.isInHideout());
+        den2.putIntoHideout(1);
+        System.out.println(den2.isInHideout());
+        System.out.println(den2.isInHideout());
+        System.out.println(den2.takeFromHideout());
+        System.out.println(den2.isInHideout());
+    }
+}
